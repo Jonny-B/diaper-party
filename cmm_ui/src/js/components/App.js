4c77b828b6bfa72a4d57163b770815061e66e6bb
Blewitt, Jonathan <blewitt.3@gmail.com>
2019-06-14 16:28:38 -0400

    Cors Stuff and getting app logo to display users on click

--- cmm_ui/src/js/components/App.js
+++ cmm_ui/src/js/components/App.js
@@ -1,38 +1,53 @@
 import React, {Component} from 'react';
+import {withStyles} from '@material-ui/core/styles'
 import {Typography} from '@material-ui/core'
-// import axios from 'axios'
+import axios from 'axios'
 // import {BrowserRouter, Link, Switch, Route, Redirect} from 'react-router-dom'
 import logo from '../../images/logo.svg';
 import '../../css/App.css';
 
-export default class App extends Component {
+export class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            users: 'Jonny B',
+            users: ['Click Logo To Find Users'],
         };
 
         this.getUsers = this.getUsers.bind(this)
     }
 
     getUsers() {
-
+        axios.get('http://cmm-api:3000/users').then(response => {
+            this.setState({users: response.data})
+        })
     }
 
     render() {
         return (
-                <div className="App">
-                    {/*<BrowserRouter>Put Content in Here</BrowserRouter>*/}
-                    <header className="App-header">
-                        <Typography>{this.state.users}</Typography>
-                        <img src={logo} className="App-logo" alt="logo"/>
-                    </header>
-                    {/*<Switch>*/}
-                        {/*<Route path={'/'} component={App}/>*/}
-                    {/*</Switch>*/}
-                </div>
+            <div className="App" id={"app"}>
+                {/*<BrowserRouter>Put Content in Here</BrowserRouter>*/}
+                <header className="App-header" id={"appHeader"}>
+                    {this.state.users.map((user, i) => {
+                        return <Typography key={`user${i}`}>{user}</Typography>
+                    })}
+                    <div className={this.props.classes.logoDiv} id={"logoDiv"} onClick={this.getUsers}>
+                        <img src={logo} id={"logoImage"} className="App-logo" alt="logo"/>
+                    </div>
+                </header>
+                {/*<Switch>*/}
+                {/*<Route path={'/'} component={App}/>*/}
+                {/*</Switch>*/}
+            </div>
         );
     }
 
 }
+
+const styles = {
+    logoDiv: {
+      margin: '15px'
+  }
+};
+
+export default withStyles(styles)(App)
